250169d2ece1ba00dbf536a8ac9b2dd994afecb0
sam <samuel.klein@online.de>
2017-10-30 23:58:32 +0100

    added solution for translating rna into protein

---
+++ bioinformaticsStronghold/prot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+
+filepath = sys.argv[1]
+file = open(filepath, "r")
+
+map = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y",
+    "UGU":"C", "UGC":"C", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+
+rnaString = file.readline().strip()
+
+for i in range(len(rnaString))[::3]:
+    triplett = rnaString[i]+rnaString[i+1]+rnaString[i+2]
+    if triplett in map:
+        sys.stdout.write(map[triplett])
+print()
